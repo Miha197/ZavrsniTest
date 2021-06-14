dfad9f91a1e7fd8a0ff593789e92cb75cf6bc530
stojanovicmihajlo5@gmail.com <stojanovicmihajlo5@gmail.com>
2021-06-14 15:41:43 +0200

    ZavrsniTest2

--- ZavrsniTest/ShopQA.cs
+++ ZavrsniTest/ShopQA.cs
@@ -52,7 +52,6 @@ namespace ZavrsniTest
         [Category("http://test.qa.rs/")]
         public void TestAddToCart()
         {
-            string packagequantity = "5";
             Logger.beginTest("TestAddToCart");
             Logger.log("INFO", "Begin Test:");
 
@@ -67,7 +66,9 @@ namespace ZavrsniTest
             LoginSubmit.Click();
             this.ExplicitWait(250);
 
+
             IWebElement InsertNumber1 = this.MyFindElement(By.XPath("//INPUT[@type='number']/self::INPUT"));
+            InsertNumber1.Click();
             InsertNumber1.SendKeys("2");
             this.ExplicitWait(250);
 
@@ -81,11 +82,53 @@ namespace ZavrsniTest
             DropDownSubmit.Click();
             this.ExplicitWait(250);
 
+
+            IWebElement ContinueShoppingButton = this.MyFindElement(By.XPath("//a[contains(.,'Continue shopping')]"));
+            ContinueShoppingButton.Click();
+
+            IWebElement DoubleBSubmit = this.MyFindElement(By.XPath("(//INPUT[@type='submit'])[2]"));
+            DoubleBSubmit.Click();
+
+            IWebElement ContinueShoppingButton2 = this.MyFindElement(By.XPath("//a[contains(.,'Continue shopping')]"));
+            ContinueShoppingButton2.Click();
+
+            IWebElement MegaBSubmit = this.MyFindElement(By.XPath("(//INPUT[@type='submit'])[3]"));
+            MegaBSubmit.Click();
+
+            IWebElement TotalCart = this.MyFindElement(By.XPath("//td[contains(., 'Total:')]"));
+            string TotalCartS = TotalCart.Text.Substring(7);
+            Logger.log("INFO:", $"Cart price:{TotalCartS}");
+
+
             IWebElement SubmitCheckout = this.MyFindElement(By.Name("checkout"));
             SubmitCheckout.Click();
 
-            IWebElement h1cart = this.WaitForElement(EC.ElementIsVisible(By.XPath("//h1[contains(text(),'Order #')]")));
+            IWebElement CardCharged = this.MyFindElement(By.XPath("//h3[contains(.,'Your credit card has been charged')]"));
+            string NewCardCharged = CardCharged.Text.Substring(CardCharged.Text.IndexOf("$"));
+            Logger.log("INFO:", $"CardChargedNew:{NewCardCharged}");
+            Assert.AreEqual(TotalCartS,NewCardCharged);
            
+        }
+        [Test]
+        [Category("http://test.qa.rs/")]
+        public void LogoutTest()
+        {
+            Logger.beginTest("LogoutTest");
+            Logger.log("INFO", "Begin Test:");
+            this.GoToURL("http://test.qa.rs/");
+
+            IWebElement LinkLogin = this.MyFindElement(By.LinkText("Login"));
+            LinkLogin.Click();
+
+            PopulateInput(By.Name("username"), "Miha.Zavrsni");
+            PopulateInput(By.Name("password"), "someComplicatedPassword");
+            IWebElement LoginSubmit = this.MyFindElement(By.Name("login"));
+            LoginSubmit.Click();
+
+            IWebElement LogoutLink = this.WaitForElement(EC.ElementIsVisible(By.PartialLinkText("Logout")));
+            Assert.IsTrue(LogoutLink.Displayed);
+            Logger.endTest();
+
         }
 
         [STAThread]
